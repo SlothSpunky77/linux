1895e43f265a6a59073d7891dcc72b6ddcff73b3
Shashank <94778190+SlothSpunky77@users.noreply.github.com>
2023-09-11 12:03:48 +0530

    Update detect_tablet_mode.sh

--- detect_tablet_mode.sh
+++ detect_tablet_mode.sh
@@ -11,3 +11,21 @@ else
 	exit 0
 fi
 done
+#
+#Messing around to try to run onboard osk
+#while true
+#do
+	#echo "$(libinput debug-events --device /dev/input/event4)"
+	#echo "$(libinput debug-events --device /dev/input/event4 | grep state)"
+#	if [ "$(libinput debug-events --device /dev/input/event4 | grep state)" = "event4   SWITCH_TOGGLE           +0.000s       switch tablet-mode state 1" ]; then
+#		echo "tablet"
+#	elif [ "$(libinput debug-events --device /dev/input/event4 | grep state)" = "event4   SWITCH_TOGGLE           +0.000s       switch tablet-mode state 0" ]; then
+#		echo "laptop"
+#	else
+#		echo "unknown"
+	#if ! pgrep -x "onboard" > /dev/null; then
+	#	onboard &
+	#fi
+#	fi
+	#sleep 1
+#done
